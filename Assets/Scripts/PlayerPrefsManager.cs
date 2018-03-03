3f0aeb25b83ffaaade3f8a350b6b8aae9035a13f
Brian Lai <brianlai30@outlook.com>
2018-03-02 18:05:55 -0800

    Added grass to all levels and PlayerPrefsManager.cs

---
+++ Assets/Scripts/PlayerPrefsManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPrefsManager : MonoBehaviour {
+
+    const string MASTER_VOLUME_KEY = "master_volume";
+    const string DIFFICULTY_KEY = "difficulty";
+    const string LEVEL_KEY = "level_unlocked_";
+	
+	void Start() {
+		
+	}
+	
+	void Update() {
+		
+	}
+
+}
