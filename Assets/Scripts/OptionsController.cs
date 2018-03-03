f1cdb86c0d7373230d8e03fcf37ff1986e3bf34b
Brian Lai <brianlai30@outlook.com>
2018-03-02 18:28:59 -0800

    Added options controller

---
+++ Assets/Scripts/OptionsController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsController : MonoBehaviour {
+	
+	void Start() {
+		
+	}
+	
+	void Update() {
+		
+	}
+
+}
