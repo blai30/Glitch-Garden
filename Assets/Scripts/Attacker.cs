181711268077e68e5a6534167f5d36d3227a9b0b
Brian Lai <brianlai30@outlook.com>
2018-03-03 22:25:43 -0800

    Updated Attacker.cs

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Attacker : MonoBehaviour {
+
+    [Range(-1f, 1.5f)]
+    public float walkSpeed;
 	
 	void Start() {
 		
 	}
 	
 	void Update() {
-		
+		transform.Translate(Vector3.left * walkSpeed * Time.deltaTime);
 	}
 
 }
