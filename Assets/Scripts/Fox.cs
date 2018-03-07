509f506c2f08ed0476b6aa24f1ab13a090ea1e33
Brian Lai <brianlai30@outlook.com>
2018-03-07 15:03:57 -0800

    Added Fox.cs and Lizard.cs scripts

---
+++ Assets/Scripts/Fox.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fox : MonoBehaviour {
+
+    private Animator anim;
+	
+	void Start() {
+		anim = GetComponent<Animator>();
+	}
+	
+	void Update() {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D collider) {
+        GameObject obj = collider.gameObject;
+
+        if (!obj.GetComponent<Defender>()) {
+            return;
+        }
+
+        if (obj.GetComponent<Stone>()) {
+            anim.SetTrigger("jumpTrigger");
+        } else {
+            
+        }
+
+        Debug.Log("Fox collided with " + collider);
+    }
+
+}
