110d983c87242869d7a506776a5eed3cbb788a40
Brian Lai <brianlai30@outlook.com>
2018-03-07 17:59:36 -0800

    Added Shredder.cs

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour {
 
-    public GameObject projectile, projectileParent;
+    public GameObject projectile, projectileParent, gun;
 	
 	void Start() {
 		
@@ -13,5 +13,11 @@ public class Shooter : MonoBehaviour {
 	void Update() {
 		
 	}
+
+    private void Fire() {
+        GameObject newProjectile = Instantiate(projectile) as GameObject;
+        newProjectile.transform.parent = projectileParent.transform;
+        newProjectile.transform.position = gun.transform.position;
+    }
 
 }
