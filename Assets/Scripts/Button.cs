02fcd783a2ab010818dc26be0b7f385be4da6939
Brian Lai <brianlai30@outlook.com>
2018-03-07 19:01:27 -0800

    Buttons are clickable

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+
+    private Button[] buttonArray;
+	
+	void Start() {
+		buttonArray = GameObject.FindObjectsOfType<Button>();
+	}
+	
+	void Update() {
+		
+	}
+
+    void OnMouseDown() {
+        print(name + " pressed");
+        foreach (Button thisButton in buttonArray) {
+            thisButton.GetComponent<SpriteRenderer>().color = Color.black;
+        }
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+
+}
