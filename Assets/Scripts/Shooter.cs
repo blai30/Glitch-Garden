9639d2c02893de8813dd6606d4d8e48d8a0a2aab
Brian Lai <brianlai30@outlook.com>
2018-03-10 11:30:18 -0800

    Adde SetMyLaneSpawner() to Shooter.cs

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -7,10 +7,12 @@ public class Shooter : MonoBehaviour {
     public GameObject projectile, gun;
     private GameObject projectileParent;
     private Animator animator;
+    private Spawner myLaneSpawner;
 	
 	void Start() {
         animator = GetComponent<Animator>();
 
+    // Creates a parent if necessary
         projectileParent = GameObject.Find("Projectiles");
         if (!projectileParent) {
             projectileParent = new GameObject("Projectiles");
@@ -23,10 +25,25 @@ public class Shooter : MonoBehaviour {
         } else {
             animator.SetBool("isAttacking", false);
         }
+
+        SetMyLaneSpawner();
+        print(myLaneSpawner);
 	}
 
+    void SetMyLaneSpawner() {
+        Spawner[] spawnerArray = GameObject.FindObjectsOfType<Spawner>();
+
+        foreach (Spawner spawner in spawnerArray) {
+            if (spawner.transform.position.y == transform.position.y) {
+                myLaneSpawner = spawner;
+                return;
+            }
+        }
+        Debug.LogError(name +  " cannot find spawner in lane");
+    }
+
     bool isAttackerAheadInLane() {
-        return true;
+        
     }
 
     private void Fire() {
