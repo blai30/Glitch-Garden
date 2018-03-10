f9ffa764bd3e63ba5e540cdf97e0d603368a92cc
Brian Lai <brianlai30@outlook.com>
2018-03-10 14:50:20 -0800

    GameTimer.cs

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour {
+
+    public float levelSeconds = 100;
+    private Slider slider;
+    private AudioSource audioSource;
+    private bool isEndOfLevel = false;
+    private LevelManager levelManager;
+    private GameObject winLabel;
+	
+	void Start() {
+        slider = GetComponent<Slider>();
+        audioSource = GetComponent<AudioSource>();
+        levelManager = GameObject.FindObjectOfType<LevelManager>();
+        FindYouWin();
+    }
+
+    void FindYouWin() {
+        winLabel = GameObject.Find("Win");
+        if (!winLabel)
+        {
+            Debug.LogWarning("Please create You Win object");
+        }
+    }
+
+    void Update() {
+		slider.value = Time.timeSinceLevelLoad / levelSeconds;
+
+        bool timeIsUp = Time.timeSinceLevelLoad >= levelSeconds;
+        if (timeIsUp && !isEndOfLevel) {
+            audioSource.Play();
+            Invoke("LoadNextLevel", audioSource.clip.length);
+            isEndOfLevel = true;
+        }
+	}
+
+    void LoadNextLevel() {
+        levelManager.LoadNextLevel();
+    }
+
+}
