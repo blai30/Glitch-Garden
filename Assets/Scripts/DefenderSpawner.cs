20996e4342c237d4a265bbcdba9ac519bd65ef02
Brian Lai <brianlai30@outlook.com>
2018-03-09 23:25:32 -0800

    DefenderSpawner.cs now prints a vector2 snapped to grid

--- Assets/Scripts/DefenderSpawner.cs
+++ Assets/Scripts/DefenderSpawner.cs
@@ -4,24 +4,28 @@ using UnityEngine;
 
 public class DefenderSpawner : MonoBehaviour {
 	
-	void Start() {
-		
-	}
-	
-	void Update() {
-		
-	}
+    public Camera myCamera;
 
     void OnMouseDown() {
         print(Input.mousePosition);
-        print(CalculateWorldPointOfMouseClick());
+        print(SnapToGrid(CalculateWorldPointOfMouseClick()));
+    }
+
+    Vector2 SnapToGrid(Vector2 rawWorldPos) {
+        float newX = Mathf.RoundToInt(rawWorldPos.x);
+        float newY = Mathf.RoundToInt(rawWorldPos.y);
+        return new Vector2(newX, newY);
     }
 
     Vector2 CalculateWorldPointOfMouseClick() {
         float mouseX = Input.mousePosition.x;
         float mouseY = Input.mousePosition.y;
+        float distanceFromCamera = 10f;
 
-        
+        Vector3 weirdTriplet = new Vector3(mouseX, mouseY, distanceFromCamera);
+        Vector2 worldPos = myCamera.ScreenToWorldPoint(weirdTriplet);
+
+        return worldPos;
     }
 
 }
