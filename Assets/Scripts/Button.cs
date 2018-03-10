5fa00925eb97540135136c4f4f64051bc260e478
Brian Lai <brianlai30@outlook.com>
2018-03-10 15:27:04 -0800

    Added costs to defender buttons

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Button : MonoBehaviour {
 
     public GameObject defenderPrefab;
     public static GameObject selectedDefender;
     private Button[] buttonArray;
+    private Text costText;
 	
 	void Start() {
 		buttonArray = GameObject.FindObjectsOfType<Button>();
+
+        costText = GetComponentInChildren<Text>();
+        if (!costText) {
+            Debug.LogWarning(name + " has no cost text");
+        }
+        costText.text = defenderPrefab.GetComponent<Defender>().starCost.ToString();
 	}
 	
 	void Update() {
