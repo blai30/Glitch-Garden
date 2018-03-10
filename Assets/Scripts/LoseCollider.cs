7adb05972ce41d30c1845ec237c42cd720ce637c
Brian Lai <brianlai30@outlook.com>
2018-03-10 14:27:03 -0800

    LoseCollider now works

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseCollider : MonoBehaviour {
+
+    private LevelManager levelManager;
+	
+	void Start() {
+		levelManager = GameObject.FindObjectOfType<LevelManager>();
+	}
+	
+	void Update() {
+		
+	}
+
+    void OnTriggerEnter2D() {
+        levelManager.LoadLevel("03b Lose");
+    }
+
+}
